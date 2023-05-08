e64edd04e5714ce34918a4e152f1ab73de9e36dd
lilac <dnjeh.001@gmail.com>
2023-05-08 11:20:54 +0900

    Today's applefrash[5]'s-code's

--- appleflash/app/list/page.js
+++ appleflash/app/list/page.js
@@ -1,13 +1,19 @@
 export default function List() { 
+    let 상품 = ["Tomatoes", "Pasta", "Coconut"]
+    let 어레이=[2,3,4]
+    let wntjr = ["a[0]=100", "console.log(상품[2])", "let a=arr.map((arr[i], i)=>{})"]
     return (
       <div>
         <h4 className="title">상품 목록</h4>
-        <div className="food">
-            <h4>상품 1 $40</h4>
-        </div>
-        <div className="food">
-            <h4>상품 2 $40</h4>
-        </div>
+        {
+          상품.map((ai)=> {
+            return( 
+              <div className="food">
+                <h4>{ai} $40</h4>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }

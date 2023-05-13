ce1e5b8d1b568b3bc7c0ac89af5844c6ca34c0dc
dnjeh <ehdnj.001@gmail.com>
2023-05-14 01:05:20 +0900

    Today's after-thiRD-applefrash[9:]s'-codes'

--- appleflash/app/list/page.js
+++ appleflash/app/list/page.js
@@ -1,6 +1,9 @@
+"use client"
 import Image from "next/image"
+import { useState } from "react"
 export default function List() { 
     let 상품 = ["Tomatoes", "Pasta", "Coconut"]
+    let [수량, set수량] = useState([0, 0, 0])
     let 어레이=[2,3,4]
     let wntjr = ["a[0]=100", "console.log(상품[2])", "let a=arr.map((arr[i], i)=>{})"]
     return (
@@ -12,6 +15,17 @@ export default function List() {
               <div className="food" key={i}>
                 <img src={`/food${i}.png`} className="food-img" alt="토마토"/>
                 <h4>{ai} $40</h4>
+                <button onClick={()=>{
+                  let t = [...수량]
+                  t[i]--
+                  set수량(t)
+                  }}>-</button>
+                <span> {수량[i]} </span>
+                <button onClick={()=>{
+                  let t=[...수량]
+                  t[i]++
+                  set수량(t)
+                }}>+</button>
               </div>
             )
           })

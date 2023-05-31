c9ac2ba68e0197103870a028620921e0ab4abd33
dnjeh <ehdnj.001@gmail.com>
2023-06-01 06:25:51 +0900

    puck

--- appleforum/pages/api/post/del.js
+++ appleforum/pages/api/post/del.js
@@ -1,15 +1,24 @@
 import { connectDB } from "@/util/database"
 import { ObjectId } from "mongodb";
+import { authOptions } from '@/pages/api/auth/[...nextauth].js'
+import { getServerSession } from 'next-auth'
 
 export default async function handler(요청, 응답) {
+    let session = await getServerSession(authOptions);
     if(요청.method=="POST") {
         console.log(요청.body)
         const client = await connectDB
         const db = client.db("forum")
         try {
+            let fetchh = await db.collection("post").findOne({ _id : new ObjectId(요청.body)})
+            if(fetchh.author==session.user.email) {
             let result = await db.collection("post").deleteOne({_id : new ObjectId(요청.body)})
             console.log(result)
             return 응답.redirect(302, "/list")
+            }
+            else {
+                return 응답.status(500).json("no parms")    
+            }
         } catch (error) {
             return 응답.status(500).json("sorry")
         }
--- appleforum/pages/api/post/new.js
+++ appleforum/pages/api/post/new.js
@@ -1,9 +1,16 @@
 import { connectDB } from "@/util/database"
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/pages/api/auth/[...nextauth].js'
 
 export default async function handler(요청, 응답) {
+    let session = await getServerSession(요청, 응답, authOptions);
     if(요청.method=="POST") {
         const client = await connectDB
         const db = client.db("forum")
+        if(session) {
+            요청.body.author = session.user.email
+        }
+        else return 응답.status(500).json("there's no user")
         if(요청.body.title=="" || 요청.body.content=="") return 응답.status(200).json("??")
         try {
             let result = await db.collection("post").insertOne(요청.body)

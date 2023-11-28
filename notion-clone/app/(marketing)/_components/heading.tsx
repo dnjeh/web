228acfb01cca32ec2648db98de65f01eed14ca49
dnjeh <ehdnj.001@gmail.com>
2023-11-28 23:07:02 +0900

    Today's notion-clone[:Auth]'s source-code's

--- notion-clone/app/(marketing)/_components/heading.tsx
+++ notion-clone/app/(marketing)/_components/heading.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useConvexAuth } from "convex/react"
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
 
 import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Link } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () => {
     const { isAuthenticated, isLoading } = useConvexAuth();
@@ -20,13 +22,25 @@ export const Heading = () => {
                 better, faster work happens.
             </h3>
             {isLoading && (
+                <div className="w-full flex items-center justify-center">
                 <Spinner size="lg"/>
+                </div>
             )}
             {isAuthenticated && !isLoading && (
-            <Button>
-                Enter Jotion
-                <ArrowRight className="h-4 w-4 ml-2"/>
+            <Button asChild>
+                <Link href="/documents">
+                    Enter Jotion
+                    <ArrowRight className="h-4 w-4 ml-2"/>
+                </Link>
             </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Jotion free
+                        <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Button>
+                </SignInButton>
             )}
         </div>
     )

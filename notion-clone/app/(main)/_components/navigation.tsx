5814b793f6c3d25bc2c01f271fad8755ae027912
dnjeh <ehdnj.001@gmail.com>
2023-12-24 12:12:31 +0900

    Today's notion-clone[:Banner]'s source-code's

--- notion-clone/app/(main)/_components/navigation.tsx
+++ notion-clone/app/(main)/_components/navigation.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { ChevronLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
@@ -19,10 +19,12 @@ import {
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
   const settings = useSettings();
   const search = useSearch();
+  const params = useParams();
   const pathname = usePathname();
   const isModile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -191,6 +193,10 @@ export const Navigation = () => {
           isModile && "left-0 w-full"
         )}
       >
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed}
+          onResetWidth={resetWidth}/>
+        ) : (
         <nav className="bg-transparent px-3 py-2 w-full">
           {isCollapsed && (
             <MenuIcon
@@ -199,7 +205,8 @@ export const Navigation = () => {
               className="h-6 w-6 text-muted-foreground"
             />
           )}
-        </nav>
+          </nav>
+        )}
       </div>
     </>
   );
---
+++ notion-clone/app/(main)/_components/title.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { useMutation } from "convex/react";
+import { Doc } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { useRef, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface TitleProps {
+    initialData: Doc<"documents">;
+}
+
+export const Title = ({
+    initialData
+}: TitleProps) => {
+    const InputRef = useRef<HTMLInputElement>(null);
+    const update = useMutation(api.documents.update);
+
+    const [title, setTitle] = useState(initialData.title || "untitled");
+    const [isEditing, setIsEditing] = useState(false);
+
+
+    const enabelInput =() => {
+        setTitle(initialData.title);
+        setIsEditing(true);
+        setTimeout(()=>{
+            InputRef.current?.focus();
+            InputRef.current?.setSelectionRange(0, InputRef.current.value.length);
+        }, 0);
+    }
+
+    const disableInput = () => {
+        setIsEditing(false);
+    };
+
+    const onChange = (
+        event : React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setTitle(event.target.value);
+        update({
+            id: initialData._id,
+            title: event.target.value || "Untitled"
+        })
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+        if(event.key === "Enter") {
+            disableInput();
+        }
+    }
+    return (
+        <div className="flex items-center gap-x-1">
+            {!!initialData.icon && <p>{initialData.icon}</p>}
+            {isEditing ? (
+                <Input
+                ref={InputRef}
+                onClick={enabelInput}
+                onBlur={disableInput}
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+                value={title}
+                className="h-7 px-2 focus-visible:ring-transparent"/>
+            ) : (
+                <Button
+                onClick={enabelInput}
+                variant="ghost"
+                size="sm"
+                className="font-normal h-auto p-1"
+                >
+                    {initialData?.title}
+                </Button>
+            )}
+        </div>
+    )
+}
+
+Title.Skeleton =function TitleSkeleton() {
+    return (
+        <Skeleton className="h-9 w-20 rounded-md"/>
+    )
+}

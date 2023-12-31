501ad8d8baca9d0f898cf87607e57e5bdd039aaa
dnjeh <ehdnj.001@gmail.com>
2023-12-31 23:41:30 +0900

    Today's fimalllly-completed notion-clone's!!!

--- notion-clone/app/(main)/_components/publish.tsx
+++ notion-clone/app/(main)/_components/publish.tsx
@@ -12,7 +12,7 @@ import { api } from "@/convex/_generated/api";
 import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-import { Globe } from "lucide-react";
+import { Check, Copy, Globe } from "lucide-react";
 
 interface PublishProps {
     initialData: Doc<"documents">
@@ -51,7 +51,7 @@ export const Publish = ({
         
         const promise = update({
             id: initialData._id,
-            isPublished: true,
+            isPublished: false,
         })
             .finally(() => setIsSubmitting(false));
 
@@ -85,8 +85,39 @@ export const Publish = ({
             </PopoverTrigger>
             <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
                 {initialData.isPublished ? (
-                    <div>
-                        Published
+                    <div className="space-y-4">
+                        <div className="flex items-center gap-x-2">
+                            <Globe className="text-sky-500 animate-pulse h-4 w-4"/>
+                            <p className="text-xs font-medium text-sky-500">
+                                This note is live on web.
+                            </p>
+                        </div>
+                        <div className="flex items-center">
+                            <input
+                                className="flex-1 px-2 text-xs border rounded-l-md h-8 bg-muted truncate"
+                                value={url}
+                                disabled
+                            />
+                            <Button
+                                onClick={onCopy}
+                                disabled={copied}
+                                className="h-8 rounded-l-none"
+                            >
+                                {copied ? (
+                                    <Check className="h-4 w-4"/>
+                                ) : (
+                                    <Copy className="h-4 w-4"/>
+                                )}
+                            </Button>
+                        </div>
+                        <Button
+                            size="sm"
+                            className="w-full text-xs"
+                            disabled={isSubmitting}
+                            onClick={onUnpublish}
+                        >
+                            Unpublish
+                        </Button>
                     </div>
                 ) : (
                     <div className="flex flex-col items-center justify-center">
@@ -101,7 +132,12 @@ export const Publish = ({
                             </span>
                             <Button
                                 disabled={isSubmitting}
-                                onClick={onp}
+                                onClick={onPublish}
+                                className="w-full text-xs"
+                                size="sm"
+                            >
+                                Publish
+                            </Button>
                     </div>
                 )}
             </PopoverContent>

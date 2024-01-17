1289903025c62315278f938c43ad351b99d55e0c
dnjeh <ehdnj.001@gmail.com>
2024-01-18 05:39:15 +0900

    Today's twitter-clone[:init]'s things'

---
+++ twitter-clone/src/app/(afterlogin)/[username]/page.tsx
@@ -0,0 +1,8 @@
+export default function asjkld({ children }) {
+    return (
+      <div>
+        HomeLayout
+        {children}
+      </div>
+    )
+  }
---
+++ twitter-clone/src/app/(afterlogin)/layout.tsx
@@ -0,0 +1,8 @@
+export default function AfterLoginLayout({ children }) {
+  return (
+    <div>
+      AfterLoginLayout
+      {children}
+    </div>
+  )
+}

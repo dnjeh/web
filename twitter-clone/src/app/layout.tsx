2996f7a3e169eee1446ab0868c892a144b834b6b
dnjeh <ehdnj.001@gmail.com>
2024-02-18 22:59:04 +0900

    Today's twitter-clone[:mswComp]'s files'

--- twitter-clone/src/app/layout.tsx
+++ twitter-clone/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import styles from "./page.module.css"
+import { MSWComponent } from './_components/MSWComponent'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <MSWComponent />
         <div className={styles.container}>
         {children}
         </div>

85989260100362aef0df03c759a0ef3f4ad20b0d
dnjeh <ehdnj.001@gmail.com>
2024-02-19 22:55:20 +0900

    Today's twitter-clone[:ServerAction]'s files'

---
+++ twitter-clone/src/app/(beforelogin)/_lib/signup.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+export default async (prevState: {message: string | undefined}, formData: FormData) => {
+  if (!formData.get("id") || !(formData.get("id") as string)?.trim() ) {
+    return { message: "no_id" };
+  }
+  if (!formData.get("name")) {
+    return { message: "no_name" };
+  }
+  if (!formData.get("password")) {
+    return { message: "no_password" };
+  }
+  if (!formData.get("image")) {
+    return { message: "no_image" };
+  }
+  let f = false;
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/users`,
+      {
+        method: "post",
+        body: formData,
+        credentials: "include",
+      }
+    );
+    console.log(response.status);
+    if (response.status === 403) {
+      return { message: "user_exist" };
+    }
+    //console.log(await response.json())
+    f = true;
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+  if (f) redirect("/home");
+};

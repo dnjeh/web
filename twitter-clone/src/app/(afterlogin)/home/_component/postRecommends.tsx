46a198a4652f26d8644c37c896a838f783d3d5cc
dnjeh(Notebook) <ehdnj.001@gmail.com>
2024-04-01 17:21:44 +0900

    Today's twitter-clone[:WhyUseReactQuery]'s files'

--- twitter-clone/src/app/(afterlogin)/home/_component/postRecommends.tsx
+++ twitter-clone/src/app/(afterlogin)/home/_component/postRecommends.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 
-import { useQuery } from "@tanstack/react-query"
-import { getPostRecommend } from "../_lib/getPostRecommends"
-import Post from "../../_component/Post"
-import { Post as iPost} from "@/model/post"
+import { useQuery } from "@tanstack/react-query";
+import { getPostRecommend } from "../_lib/getPostRecommends";
+import Post from "../../_component/Post";
+import { Post as iPost } from "@/model/post";
 
 export default function PostRecommends() {
-    const {data} = useQuery<iPost[]>({queryKey: ["posts", "recommends"], queryFn: getPostRecommend})
-    return data?.map((post)=>(
-        <Post key={post.postId} post={post} />
-    ))
+  const { data } = useQuery<iPost[]>({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommend,
+    staleTime: 60000,
+  });
+  return data?.map((post) => <Post key={post.postId} post={post} />);
 }
